83ec20558adbcb1437b5e4a38bc9f49fa26a5aa3
FutureBooster93 <rezaerfani1993@gmail.com>
2023-11-24 20:13:03 +0330

    making of movie admin controller

--- Uni_Movie/Controllers/MovieController.cs
+++ Uni_Movie/Controllers/MovieController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Drawing;
+using System.Drawing.Imaging;
 using Uni_Movie.Data;
 using Uni_Movie.Models;
 using Uni_Movie.ViewModels;
@@ -14,53 +17,11 @@ namespace Uni_Movie.Controllers
 		{
 			db = _db;
 		}
-		public IActionResult Create()
+		public IActionResult Index()
 		{
-			MovieCreateViewModel model = new MovieCreateViewModel()
-			{
-				movie = new MovieViewModel(),
-				genreList = db.Genres.Select(x => new SelectListItem
-				{
-					Text = x.title,
-					Value = x.Id.ToString()
-				})
-			};
-
-			return View(model);
+			IEnumerable<Movie> movies=db.Movies.Include(x=>x.genre).ToList();
+			return View(movies);
 		}
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public IActionResult Create(MovieCreateViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				Movie movie = new()
-				{
-					Title = model.movie.Title,
-					Year = model.movie.Year,
-					Director = model.movie.Director,
-					Description = model.movie.Description,
-					Rate = model.movie.Rate,
-					genreId = model.movie.genreId
-				};
-				if (model.movie.MovieImage != null)
-				{
-					string extention = Path.GetExtension(model.movie.MovieImage.FileName).ToLower();
-					if (extention == ".jpg" || extention == ".jpeg" || extention == ".png")
-					{
-						if (model.movie.MovieImage.Length <= 5 * Math.Pow(1024, 2))
-						{
-							byte[] buffer = new byte[model.movie.MovieImage.Length];
-							model.movie.MovieImage.OpenReadStream().Read(buffer, 0, buffer.Length);
-							movie.MovieImage = buffer;
-						}
-					}
-				}
-			db.Movies.Add(movie);
-			db.SaveChanges();
-			return RedirectToAction("Index");
-			}
-			else { return View(model); }
-		}
+		
 	}
 }
